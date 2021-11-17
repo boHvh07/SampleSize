593eee34d0cfa5907163af58d01052247702d8fa
Bohvh07 <bo0709@hotmail.nl>
2021-11-17 16:20:36 +0100

    Assignment 1 Done

--- TA1.R
+++ TA1.R
@@ -34,9 +34,49 @@ r5 <-convert.chi2r(chi2=chi2r5, n=n5)
 ### Estimate Sample size
 sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
                       n1 = c(n1, n2, n3, r4$n, r5$n),
-                      weight1 = c(20, 10, 15, 10, 10))
+                      weight1 = c(100, 50, 60, 50, 50))
 
-sample4confidence(r1 = c(0.469, 0.212, 0.219, 0.324, 0.129), 
+/// 395
+
+#### Sensitivity Analysis
+#a.
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
+                  n1 = c(n1*2, n2, n3, r4$n, r5$n),
+                  weight1 = c(100, 50, 60, 50, 50))
+
+#b.
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
                   n1 = c(n1, n2, n3, r4$n, r5$n),
-                  weight1 = c(100, 50, 75, 50, 50))
+                  weight1 = c(100, 25, 38, 25, 25))
 
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
+                  n1 = c(n1, n2, n3, r4$n, r5$n),
+                  weight1 = c(100, 0, 0, 0, 0))
+
+
+#c. 
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
+                  n1 = c(n1, n2, n3, r4$n, r5$n),
+                  weight1 = c(100, 50, 60, 50, 50),
+                  r2 = .1)
+                  
+#d.
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
+                  n1 = c(n1, n2, n3, r4$n, r5$n),
+                  weight1 = c(100, 50, 60, 50, 50),
+                  r2 = .2)
+
+
+
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
+                  n1 = c(n1, n2, n3, r4$n, r5$n),
+                  weight1 = c(100, 80, 80, 80, 80))
+
+sample4confidence(r1 = c(r1, r2, r3, r4$r, r5$r), 
+                  n1 = c(n1, n2, n3, r4$n, r5$n),
+                  weight1 = c(100, 30, 30, 30, 30))
+
+
+####Conclusion
+
+            
